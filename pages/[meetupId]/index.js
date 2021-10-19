fd057778445c40d89813f92f55580396e4e91424
rmisca <rominamisca95@gmail.com>
2021-10-19 17:24:48 +0300

    Created the pages and added functionality to them

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,49 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="The meetup description"
+    />
+  );
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "First Meetup",
+        address: "Some Street 5, Some City",
+        description: "The meetup description",
+      },
+    },
+  };
+}
+
+export default MeetupDetails;
